d1cd4f0fc50cb683bad36c057a2a7438f90949e3
Alex B <bojenko.alex@gmail.com>
2019-05-02 21:34:23 -0500

    add Wildcard Matching problem

--- Wildcard Matching - LeetCode.py
+++ Wildcard Matching - LeetCode.py
@@ -1,11 +1,106 @@
+from typing import List
+
 
 class Solution:
 
     def isMatch(self, s: str, p: str) -> bool:
-        return match(s, p, 0, 0)
+        # return recursive_match(s, p, 0, 0)
+        # NFA
+        digraph = build_epsilon_transition_digraph(p)
+        return recognizes(digraph, s, p)
 
 
-def match(s: str, p: str, si: int, pi: int) -> bool:
+# NFA simulation
+def recognizes(digraph: 'Digraph', s: str, p: str):
+    # states reachable from start by epsilon-transitions
+    pc = []
+    dfs = DirectedDFS(digraph, [0])
+    for v in range(digraph.vertices_count):
+        if dfs.visited(v):
+            pc.append(v)
+
+    for i in range(len(s)):
+        # states reachable after scanning past s[i]
+        match = []
+        for v in pc:
+            if v == len(p):
+                continue
+            if p[v] == s[i] or p[v] == '?' or p[v] == '*':
+                match.append(v + 1)
+
+        # follow epsilon transition
+        pc = []
+        dfs = DirectedDFS(digraph, match)
+        for v in range(digraph.vertices_count):
+            if dfs.visited(v):
+                pc.append(v)
+
+    for v in pc:
+        if v == len(p):
+            return True
+    return False
+
+
+# NFA construction
+def build_epsilon_transition_digraph(p: str) -> 'Digraph':
+    digraph = Digraph(len(p) + 1)  # last state is end state
+    for i in range(len(p)):
+        if p[i] == '(' or p[i] == ')':
+            digraph.add_edge(i, i + 1)
+        if p[i] == '*':
+            digraph.add_edge(i, i + 1)
+            digraph.add_edge(i + 1, i)
+    return digraph
+
+
+class Digraph:
+    # states are numerical numbers start from 0 to V-1
+    def __init__(self, vertices_count: int):
+        """
+        create empty graph with V vertices
+        """
+        self.vertices_count = vertices_count
+        # Adjacency-list, maintain vertex-indexed array of lists.
+        self.adj = [[] for _ in range(vertices_count)]
+
+    def add_edge(self, v, w):
+        """
+        add a directed edge v->w
+        """
+        self.adj[v].append(w)
+
+
+class DirectedDFS:
+    def __init__(self, digraph: Digraph, s: List[int]):
+        """
+        :type digraph: Digraph
+        :type s: List[int] start states
+        """
+        self.__marked = [False for _ in range(digraph.vertices_count)]
+        for v in s:
+            if not self.__marked[v]:
+                self.__dfs(digraph, v)
+
+    def __dfs(self, digraph: Digraph, v: int):
+        """
+        recursive DFS does the work
+        """
+        self.__marked[v] = True
+        for w in digraph.adj[v]:
+            if not self.__marked[w]:
+                self.__dfs(digraph, w)
+
+    def visited(self, v: int):
+        """
+        client can ask whether any vertex is reachable from s
+        """
+        return self.__marked[v]
+
+
+def recursive_match(s: str, p: str, si: int, pi: int) -> bool:
+    """
+    Brute Force recursive solution
+    """
     # end of s
     if len(s) <= si:
         # end of p or only *'s left
@@ -19,21 +114,21 @@ def match(s: str, p: str, si: int, pi: int) -> bool:
 
     # *
     if p[pi] == '*':
-        if match(s, p, si+1, pi):
+        if recursive_match(s, p, si+1, pi):
             return True
-        if match(s, p, si+1, pi+1):
+        if recursive_match(s, p, si+1, pi+1):
             return True
-        if match(s, p, si, pi+1):
+        if recursive_match(s, p, si, pi+1):
             return True
         return False
 
     # ?
     if p[pi] == '?':
-        return match(s, p, si+1, pi+1)
+        return recursive_match(s, p, si+1, pi+1)
 
     # character
     if s[si] == p[pi]:
-        return match(s, p, si+1, pi+1)
+        return recursive_match(s, p, si+1, pi+1)
     return False
 
 
