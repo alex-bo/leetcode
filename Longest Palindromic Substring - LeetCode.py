5f8af011ec3530b1ab70d9c7c2373b0979964e3d
Alex B <bojenko.alex@gmail.com>
2019-05-03 22:13:36 -0500

    add Longest Palindromic Substring - LeetCode.py

---
+++ Longest Palindromic Substring - LeetCode.py
@@ -0,0 +1,42 @@
+
+
+class Solution:
+
+    def longestPalindrome(self, s: str) -> str:
+        mx = ''
+        for i in range(len(s)):
+            mx = max(
+                find_palindrome(s, i - 1, i),
+                find_palindrome(s, i, i),
+                mx,
+                key=lambda ss: len(ss)
+            )
+        return mx
+
+
+def find_palindrome(s: str, left: int, right: int) -> int:
+    res = ''
+    for j in range(len(s)):
+        lft = left - j
+        rgt = right + j
+        if lft < 0 or rgt >= len(s) or s[lft] != s[rgt]:
+            break
+        res = s[lft:rgt + 1]
+    return res
+
+
+def test_one(s: int, expected: str):
+    print(s)
+    actual = Solution().longestPalindrome(s)
+    if len(actual) == len(expected):
+        print('OK')
+    else:
+        print('WRONG!')
+
+
+def test():
+    test_one('babad', 'bab')
+
+
+if __name__ == '__main__':
+    test()
